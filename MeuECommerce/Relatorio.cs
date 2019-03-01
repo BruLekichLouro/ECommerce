16e92615636ea1cc6a8c13b9162f839cd362cb19
BruLekichLouro <bububio@yahoo.com.br>
2019-03-01 15:25:13 -0300

    Adicionando recursos e iniciando banco de dados

---
+++ MeuECommerce/Relatorio.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeuECommerce
+{
+    public class Relatorio
+    {
+        private readonly Catalogo catalogo;
+
+        public Relatorio(Catalogo catalogo)
+        {
+            this.catalogo = catalogo;
+        }
+    }
+}
