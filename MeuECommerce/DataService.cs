0c1cdb61ea4ed2e19718a2865304c232e3422eea
BruLekichLouro <bububio@yahoo.com.br>
2019-03-13 18:57:07 -0300

    Lendo arquivo json de livros

--- MeuECommerce/DataService.cs
+++ MeuECommerce/DataService.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -16,6 +18,15 @@ namespace MeuECommerce
         public void InicializaDB()
         {
             contexto.Database.EnsureCreated();
+
+            var json = File.ReadAllText("livros.json");
+            var livros = JsonConvert.DeserializeObject<List<Livro>>(json);
         }
+    }
+    class Livro
+    {
+        public string Codigo { get; set; }
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
     }
 }
--- MeuECommerce/Startup.cs
+++ MeuECommerce/Startup.cs
@@ -29,6 +29,8 @@ namespace MeuECommerce
             string connectionString = Configuration.GetConnectionString("Default");
             services.AddDbContext<ApplicationContext>(options => 
             options.UseSqlServer(connectionString));
+
+            services.AddTransient<IDataService, DataService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -53,7 +55,8 @@ namespace MeuECommerce
                     name: "default",
                     template: "{controller=Pedido}/{action=Carrossel}/{id?}");
             });
-            serviceProvider.GetService<ApplicationContext>().Database.Migrate();
+            serviceProvider.GetService<IDataService>().InicializaDB();
+
         }
     }
 }
